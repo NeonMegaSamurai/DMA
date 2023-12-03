6db4b046febdefb74930670fb6a61b0323606a4f
Michelle <shavmn17@gmail.com>
2023-12-03 14:03:36 +0700

    GUI prealfa

--- src/GUI.java
+++ src/GUI.java
@@ -3,21 +3,30 @@ import java.awt.*;
 
 public class GUI extends JFrame {
     public GUI(){
-        setSize(500,500);
+        setSize(500,200);
         setLocation(500,250);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
+        setResizable(false);
 
         JPanel ButtonMenu = new JPanel(new GridLayout(3,1,5,5));
-        ButtonMenu.add(new JButton("1"));
-        ButtonMenu.add(new JButton("2"));
-        ButtonMenu.add(new JButton("3"));
+        ButtonMenu.add(new JButton("Products"));
+        ButtonMenu.add(new JButton("Availability of products"));
+        ButtonMenu.add(new JButton("Sales statistics"));
+
+        JPanel TextMenu = new JPanel(new GridLayout(2,1,5,5));
+        TextMenu.add(new JLabel("Hello!"));
+        TextMenu.add(new JLabel("Successful work. Select the one you need"));
 
         JPanel front = new JPanel(new FlowLayout(FlowLayout.CENTER));
         front.add(ButtonMenu);
+        JPanel front1 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        front1.add(TextMenu);
 
         Container container = getContentPane();
         container.add(front, BorderLayout.SOUTH);
+        container.add(front1, BorderLayout.NORTH);
+
         setVisible(true);
     }
 }
